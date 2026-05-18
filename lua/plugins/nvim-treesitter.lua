d75f131be738db1dfa2e589325412084bfc39d59
TynK-M <matteo.pauroso@gmail.com>
2026-05-19 00:53:18 +0200

    First commit

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,72 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.config").setup({
+      ensure_installed = {
+        "lua", 
+        "vim", 
+        "vimdoc", 
+        "bash", 
+        "python", 
+        "javascript",
+        "typescript", 
+        "html", 
+        "css", 
+        "json", 
+        "c", 
+        "cpp"
+      },
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+
+      indent = {
+        enable = true,
+      },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          node_incremental = "<CR>",
+          node_decremental = "<BS>",
+        },
+      },
+
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]c"] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[c"] = "@class.outer",
+          },
+        },
+      },
+
+      rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = 3000,
+      },
+    })
+  end,
+}
