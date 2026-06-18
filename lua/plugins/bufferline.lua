776c45286d3ecd173ae5ed9a6f7340fb93c220db
TynK-M <matteo.pauroso@gmail.com>
2026-06-18 21:37:32 +0200

    chore: update plugins

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,5 +7,6 @@ return {
 
 		vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { silent = true })
 		vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { silent = true })
+		vim.keymap.set("n", "<leader>bd", ":bd<CR>", { silent = true })
 	end,
 }
