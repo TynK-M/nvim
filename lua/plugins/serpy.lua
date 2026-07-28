30b2b4fd1519f29b9db21a7bf5f6d9f9ae92ef2b
TynK-M <matteo.pauroso@gmail.com>
2026-07-28 16:30:58 +0200

    chore: update plugins

--- lua/plugins/serpy.lua
+++ lua/plugins/serpy.lua
@@ -2,6 +2,21 @@ return {
 	"TynK-M/nvim-serpy",
 
 	config = function()
-		require("serpy").setup()
+		require("serpy").setup({
+			languages = {
+				python = { enabled = true },
+			},
+
+			keymaps = {
+				enabled = true,
+
+				pyrun_current = "<leader>pc",
+				pyrun_current_with_flags = "<leader>pf",
+				pydoc = "<leader>pd",
+				pydoc_current_word = "<leader>pw",
+
+				luarun_current = "<leader>lc",
+			},
+		})
 	end,
 }
