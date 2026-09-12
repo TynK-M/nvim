8b16f99eb62b00e7ca13da2d76c060b251b2d0ce
TynK-M <matteo.pauroso@gmail.com>
2026-09-12 15:07:36 +0200

    feat: add telescope keybindings
    Adds keybindings for telescope for faster use.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,4 +6,14 @@ return {
 		-- optional but recommended
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
+
+	keys = {
+		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+		{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
+		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+		{ "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
+		{ "<leader>fc", "<cmd>Telescope commands<cr>", desc = "Commands" },
+		{ "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
+	},
 }
